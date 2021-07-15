5c440e7e346d526bbad9e04fe57e144fb246a569
Sydni <sydnimpierce@gmail.com>
2021-07-15 14:40:35 -0400

    Updated mongo connection code

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import mission_to_mars
+import pymongo
+from mission_to_mars import scraping_results
 
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
-mongo = PyMongo(app)
+# Use pymongo to set up mongo connection
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+db = client.mars
+
+db.latest_data.drop()
+
+db.latest_data.insert(scraping_results)
 
 @app.route("/")
 def index():
